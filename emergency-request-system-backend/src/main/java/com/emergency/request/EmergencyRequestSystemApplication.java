60ab2323a732e96d2b76e302abefd1a7386eb211
Theres Paul <therespaul77@gmail.com>
2026-04-01 23:10:16 +0530

    Initial commit

---
+++ emergency-request-system-backend/src/main/java/com/emergency/request/EmergencyRequestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.emergency.request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyRequestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyRequestSystemApplication.class, args);
+	}
+
+}
